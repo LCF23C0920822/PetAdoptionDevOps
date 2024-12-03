e1434f727c5ccdbc9508b87689102165c426cff9
Miguel Poma <miguelpoma2909@gmail.com>
2024-12-02 23:31:20 -0500

    test files updates

--- __tests__/dbConnection.test.js
+++ __tests__/dbConnection.test.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 beforeAll(async () => {
   // Connect to MongoDB using the environment URI
-  await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+  await mongoose.connect(process.env.MONGO_URI);
 });
 
 afterAll(async () => {
